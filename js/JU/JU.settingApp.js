9448f6d9c346df257cbf9efb81b5c73d95b45885
gilad1987 <gilad1987@gmail.com>
2015-10-19 18:50:03 -0700

    .

--- js/JU/JU.settingApp.js
+++ js/JU/JU.settingApp.js
@@ -1,3 +1,16 @@
 /**
  * Created by giladtakoni on 10/19/15.
  */
+
+(function(window){
+
+    var setting = {};
+
+    setting.hideMenuWhenScrollDown = true;
+    setting.menuSticky = true;
+    setting.menuSticky = true;
+
+
+
+    window.JUsettingApp = setting;
+})(window);
