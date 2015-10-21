bf7160214523ee8803d3264bbc69ce877e589f9f
gilad1987 <gilad1987@gmail.com>
2015-10-21 17:41:18 +0300

    .

--- contact.php
+++ contact.php
@@ -33,12 +33,76 @@ $version = rand(0,10000000000000);
 </head>
 <body class="">
 
-<article>
+<article class="max-width-content contact-page">
 
-    <h3 class="page sub-title">
+    <h1 class="blue">Contact us</h1>
 
+    <h3 class="page sub-title blue">
+        We’re here to help. Please choose from the contact options below. You can also find answers to frequently asked questions (FAQ) by clicking on the boxes below.
     </h3>
 
+    <div class="form">
+        <div class="title blue">
+            Email us
+        </div>
+
+        <div class="form-wrapper">
+            <div class="ju-input-wrapper">
+                <label>First name</label>
+                <input type="text">
+            </div>
+
+            <div class="ju-input-wrapper">
+                <label>Last name</label>
+                <input type="text">
+            </div>
+
+            <div class="ju-input-wrapper">
+                <label>Email address</label>
+                <input type="text">
+            </div>
+
+            <div class="ju-input-wrapper">
+                <label>Subject</label>
+                <input type="text">
+            </div>
+
+            <div class="ju-input-wrapper textarea">
+                <label>Enter comment (1000 characters maximum)</label>
+                <textarea></textarea>
+            </div>
+        </div>
+    </div>
+
+    <div class="details-contact">
+
+        <div class="call-us">
+            <div class="title blue">
+                Call us
+            </div>
+            <div>
+                Call toll free at:
+            </div>
+            <div>
+                (888) 515 5292
+            </div>
+        </div>
+
+        <div class="write-to-us">
+            <div class="title blue">
+                Write to us
+            </div>
+
+            <div>Jerusalem U</div>
+            <div>11110 W Oakland Park Blvd Suite 288</div>
+            <div>Sunrise, FL 33351 - 6808</div>
+        </div>
+
+    </div>
+
+    <div class="clear"></div>
+
+    <div class="note blue"><span class="bold">PLEASE NOTE:</span>  The answer to your question may already appear in our FAQ pages:</div>
 </article>
 </body>
 </html>
